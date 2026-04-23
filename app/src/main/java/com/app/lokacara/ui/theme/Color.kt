ffd145071297ac65204b0a576c68a5d34c1887ea
ZyrooxxSin <zuljuhdinurnubli24@gmail.com>
2026-04-23 22:36:10 +0700

    first commit

---
+++ app/src/main/java/com/app/lokacara/ui/theme/Color.kt
@@ -0,0 +1,57 @@
+package com.app.lokacara.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+
+val Primary100 = Color(0xFFD4E4FF)
+val Primary200 = Color(0xFFA6C8FF)
+val Primary300 = Color(0xFF7AA9FF)
+val Primary400 = Color(0xFF538CFF)
+val Primary500 = Color(0xFF2563EB)
+val Primary600 = Color(0xFF1D4ED8)
+val Primary700 = Color(0xFF1E3A8A)
+val Primary800 = Color(0xFF0B1F51)
+val Primary900 = Color(0xFF040E29)
+
+
+val Secondary100 = Color(0xFFFEF3C7)
+val Secondary200 = Color(0xFFFDE68A)
+val Secondary300 = Color(0xFFFCD34D)
+val Secondary400 = Color(0xFFFBBF24)
+val Secondary500 = Color(0xFFF59E0B)
+val Secondary600 = Color(0xFFD97706)
+val Secondary700 = Color(0xFFB45309)
+val Secondary800 = Color(0xFF78350F)
+val Secondary900 = Color(0xFF451A03)
+
+
+val Gray100 = Color(0xFFF3F4F6)
+val Gray200 = Color(0xFFE5E7EB)
+val Gray300 = Color(0xFFD1D5DB)
+val Gray400 = Color(0xFF9CA3AF)
+val Gray500 = Color(0xFF6B7280)
+val Gray600 = Color(0xFF4B5563)
+val Gray700 = Color(0xFF374151)
+val Gray800 = Color(0xFF1F2937)
+val Gray900 = Color(0xFF111827)
+
+
+val SemanticErrorBase = Color(0xFFDC2626)
+val SemanticErrorLight = Color(0xFFFEE2E2)
+val SemanticErrorInteraction = Color(0xFFF87171)
+
+val SemanticWarningBase = Color(0xFFFACC15)
+val SemanticWarningLight = Color(0xFFFEF9C3)
+val SemanticWarningInteraction = Color(0xFFFDE047)
+
+val SemanticSuccessBase = Color(0xFF84CC16)
+val SemanticSuccessLight = Color(0xFFECFCCB)
+val SemanticSuccessInteraction = Color(0xFFBEF264)
+
+val SemanticNeutralBase = Color(0xFF9CA3AF)
+val SemanticNeutralLight = Color(0xFFF3F4F6)
+val SemanticNeutralInteraction = Color(0xFFD1D5DB)
+
+val SemanticInfoBase = Color(0xFF3B82F6)
+val SemanticInfoLight = Color(0xFFDBEAFE)
+val SemanticInfoInteraction = Color(0xFF93C5FD)
